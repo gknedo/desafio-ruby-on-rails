a5deed3f7eb44799aa8d6424241e66c02949210a
Canedo <canedo@webgoal.com.br>
2020-09-08 20:25:11 -0300

    adiciona batch import defaults

--- app/models/batch_import.rb
+++ app/models/batch_import.rb
@@ -1,2 +1,13 @@
 class BatchImport < ApplicationRecord
+  before_create :set_defaults
+
+  enum status: {
+    processing: 1,
+    completed: 2
+  }
+
+  private
+  def set_defaults
+    self.status ||= 1
+  end
 end
--- spec/models/batch_import_spec.rb
+++ spec/models/batch_import_spec.rb
@@ -1,4 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe BatchImport, type: :model do
+  describe 'ao verificar o status de uma importação' do
+    subject { batch_import.reload.status.to_sym }
+    let!(:batch_import) { create(:batch_import, status: status) }
+
+    context 'sem status' do
+      let!(:status) { nil }
+
+      it 'retorna em processamento' do
+        expect(subject).to eq :processing
+      end
+    end
+
+    context 'com status em processamento' do
+      let!(:status) { 1 }
+
+      it 'retorna em processamento' do
+        expect(subject).to eq :processing
+      end
+    end
+
+    context 'com status finalizado' do
+      let!(:status) { 2 }
+
+      it 'retorna em processamento' do
+        expect(subject).to eq :completed
+      end
+    end
+  end
 end
